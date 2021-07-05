43d5896670fba1ddafda66bc5f6ab75ee44854f0
郭晓东 <guoxiaodong@corp.netease.com>
2021-07-05 21:37:26 +0800

    no message

--- app/src/main/java/com/demo/compose/case/SimpleCase.kt
+++ app/src/main/java/com/demo/compose/case/SimpleCase.kt
@@ -1,21 +1,22 @@
 package com.demo.compose.case
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.demo.compose.R
 
 @Preview
 @Composable
@@ -29,12 +30,32 @@ fun CheckBoxCase() {
 
 @Preview
 @Composable
-fun FloatingActionButtonCase() {
-    ExtendedFloatingActionButton(
-        icon = { Icon(Icons.Filled.Favorite, "") },
-        text = { Text("FloatingActionButton") },
-        onClick = { /*do something*/ },
-        elevation = FloatingActionButtonDefaults.elevation(8.dp)
+fun SwitchCase() {
+    val checkedState = remember { mutableStateOf(true) }
+    Switch(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
+    )
+}
+
+@Preview
+@Composable
+fun IconCase() {
+    Icon(Icons.Rounded.Home, contentDescription = "")
+}
+
+@Preview
+@Composable
+fun ImageCase() {
+    Image(
+        painterResource(R.drawable.arctic_fox),
+        null,
+        Modifier
+            .size(256.dp)
+            .padding(16.dp)
+            .shadow(16.dp, CircleShape)
+            .border(6.dp, MaterialTheme.colors.primary, CircleShape),
+        contentScale = ContentScale.Crop
     )
 }
 
@@ -82,42 +103,6 @@ fun SliderCase() {
 
 @Preview
 @Composable
-fun SnackbarCase() {
-    Column {
-        val snackbarVisibleState = remember { mutableStateOf(false) }
-
-        Button(onClick = { snackbarVisibleState.value = !snackbarVisibleState.value }) {
-            if (snackbarVisibleState.value) {
-                Text("Hide Snackbar")
-            } else {
-                Text("Show Snackbar")
-            }
-        }
-        if (snackbarVisibleState.value) {
-            Snackbar(
-                action = {
-                    Button(onClick = {}) {
-                        Text("MyAction")
-                    }
-                },
-                modifier = Modifier.padding(8.dp)
-            ) { Text(text = "This is a snackbar!") }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun SwitchCase() {
-    val checkedState = remember { mutableStateOf(true) }
-    Switch(
-        checked = checkedState.value,
-        onCheckedChange = { checkedState.value = it }
-    )
-}
-
-@Preview
-@Composable
 fun TextFieldCase() {
     Column(Modifier.padding(16.dp)) {
         val textState = remember { mutableStateOf(TextFieldValue()) }
@@ -126,33 +111,5 @@ fun TextFieldCase() {
             onValueChange = { textState.value = it }
         )
         Text("The textfield has this text: " + textState.value.text)
-    }
-}
-
-@Preview
-@Composable
-fun TopAppBarCase() {
-    Column {
-        TopAppBar(
-            elevation = 4.dp,
-            title = {
-                Text("I'm a TopAppBar")
-            },
-            backgroundColor = MaterialTheme.colors.primarySurface,
-            navigationIcon = {
-                IconButton(onClick = {/* Do Something*/ }) {
-                    Icon(Icons.Filled.ArrowBack, null)
-                }
-            }, actions = {
-                IconButton(onClick = {/* Do Something*/ }) {
-                    Icon(Icons.Filled.Share, null)
-                }
-                IconButton(onClick = {/* Do Something*/ }) {
-                    Icon(Icons.Filled.Settings, null)
-                }
-            })
-
-        Text("Hello World")
-
     }
 }
