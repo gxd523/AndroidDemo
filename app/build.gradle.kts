4c812d8b91dbfa303f52d55f6fdb9e9748bd14fd
郭晓东 <guoxiaodong@corp.netease.com>
2024-09-11 11:48:24 +0800

    feat:使用BoM(Bill of Materials)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,17 +26,25 @@ android {
     buildFeatures {
         compose = true
     }
+    composeCompiler {
+        // 配置Compose编译器
+    }
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+
     implementation(libs.coil.compose)
     implementation(libs.landscapist.coil)
     implementation(libs.coil.svg)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
