1db031148c46113ea2f1efa8c3be86fa5d10439c
郭晓东 <guoxiaodong@corp.netease.com>
2024-10-23 16:35:14 +0800

    optimize:format

--- app/src/main/java/com/gxd/demo/compose/ui/theme/Color.kt
+++ app/src/main/java/com/gxd/demo/compose/ui/theme/Color.kt
@@ -3,14 +3,6 @@ package com.gxd.demo.compose.ui.theme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.colorspace.ColorSpaces
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val white = Color(0xFFFFFFFF).convert(ColorSpaces.Oklab)
 val white1 = Color(0xFFF7F7F7).convert(ColorSpaces.Oklab)
 val white2 = Color(0xFFEDEDED).convert(ColorSpaces.Oklab)
