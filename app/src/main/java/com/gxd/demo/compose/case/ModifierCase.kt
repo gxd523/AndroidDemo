1b0fb1c534799a26301db9bc50e31109f768725e
郭晓东 <guoxiaodong@corp.netease.com>
2024-11-19 10:50:01 +0800

    feat:ModifierCase

--- app/src/main/java/com/gxd/demo/compose/case/ModifierCase.kt
+++ app/src/main/java/com/gxd/demo/compose/case/ModifierCase.kt
@@ -1,7 +1,9 @@
 package com.gxd.demo.compose.case
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
@@ -16,10 +18,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * 自定义Modifier示例
@@ -68,10 +73,13 @@ fun LayoutCase(padding: Dp = 20.dp) {
 @Preview
 @Composable
 private fun DrawModifierCase() {
-    Box(Modifier.size(100.dp).background(Color.Red).drawWithContent {
+    Box(Modifier.size(200.dp).background(Color.Red).drawWithContent {
+        drawCircle(Color.Green)
         drawContent()
-        drawCircle(Color.Blue, 10.dp.toPx(), center)
-    }.background(Color.Green))
+        drawCircle(color = Color.Yellow, radius = size.minDimension / 3f)
+    }.padding(20.dp).background(Color.Magenta)) {
+        Text("123", color = Color.White, fontSize = 50.sp)
+    }
 }
 
 @Preview(showBackground = true)
@@ -86,6 +94,28 @@ private fun PaddingCase() {
                 .requiredSize(40.dp)
         )
     }
+}
+
+@Preview
+@Composable
+fun PointerInputCase() {
+    val context = LocalContext.current
+    Box(
+        Modifier
+            .background(Color.Blue)
+            .size(200.dp)
+            .background(Color.Red)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    Toast.makeText(context, "单击", Toast.LENGTH_SHORT).show()
+                }, onLongPress = {
+                    Toast.makeText(context, "长按", Toast.LENGTH_SHORT).show()
+                }, onDoubleTap = {
+                    Toast.makeText(context, "双击", Toast.LENGTH_SHORT).show()
+                })
+            }
+            .padding(50.dp)
+    )
 }
 
 /**
