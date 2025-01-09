831df154f14540816fb1928738df6a431032bfab
郭晓东 <guoxiaodong@corp.netease.com>
2025-01-09 20:32:58 +0800

    feat:App Links

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,32 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
+class MyPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        val extension = target.extensions.create("myExtension", MyExtension::class.java)
+        target.beforeEvaluate {
+            println("beforeEvaluate = ${extension.name}")
+        }
+        target.afterEvaluate {
+            println("afterEvaluate = ${extension.name}")
+        }
+    }
+}
+
+open class MyExtension {
+    var name = "abc"
+}
+
+apply<MyPlugin>()
+
+myExtension {
+    name = "efg"
+}
+
+fun Project.myExtension(action: MyExtension.() -> Unit) {
+    extensions.configure<MyExtension>("myExtension", action)
+}
+
 android {
     namespace = "com.gxd.demo.compose"
     compileSdk = 35
@@ -15,6 +41,28 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
+
+    signingConfigs {
+        create("sign_config") {
+            storeFile = file(System.getProperty("user.home") + "/.android/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("sign_config")
+        }
+        release {
+            isMinifyEnabled = true
+            isDebuggable = false
+            signingConfig = signingConfigs.getByName("sign_config")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
 
     buildTypes {
@@ -49,6 +97,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.activity)
 
     implementation(libs.coil.compose)
     implementation(libs.coil.okhttp)
@@ -58,6 +108,9 @@ dependencies {
 
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
+
+//    debugImplementation(libs.leakcanary)
+//    releaseImplementation(libs.leakcanary.no.op)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
---
+++ app/src/main/java/com/gxd/demo/compose/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.gxd.demo.compose
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
