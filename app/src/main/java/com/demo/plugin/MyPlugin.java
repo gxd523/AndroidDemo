082a75d8f46ae7cf3cff687af8cb57074a64725d
郭晓东 <guoxiaodong@corp.netease.com>
2025-02-12 14:59:49 +0800

    feat:插件化、热修复

---
+++ app/src/main/java/com/demo/plugin/MyPlugin.java
@@ -0,0 +1,5 @@
+package com.demo.plugin;
+
+public class MyPlugin {
+    public static final String MY_PLUGIN = "GET PLUGIN FAILED";
+}
---
+++ app/src/main/java/com/gxd/demo/compose/util/FileExt.kt
@@ -0,0 +1,15 @@
+package com.gxd.demo.compose.util
+
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
+fun InputStream.tosByteArray(): ByteArray = use { inputStream ->
+    ByteArrayOutputStream().use { outputStream ->
+        val buffer = ByteArray(1024)
+        var readLength = 0
+        while (inputStream.read(buffer).also { readLength = it } > 0) {
+            outputStream.write(buffer, 0, readLength)
+        }
+        outputStream.toByteArray()
+    }
+}
