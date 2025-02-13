3ea19c3f729e19c29dd68462b5e41167348910f6
郭晓东 <guoxiaodong@corp.netease.com>
2025-02-13 18:43:38 +0800

    optimize:nothing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,12 +74,6 @@ android {
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
